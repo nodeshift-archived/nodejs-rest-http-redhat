4c78aedbae985e11aa041e71763ed83436b14435
Helio Frota <00hf11@gmail.com>
2019-09-25 16:05:51 -0300

    chore: update to use RHSCL image

--- test/integration/greeting-integration-test.js
+++ test/integration/greeting-integration-test.js
@@ -6,7 +6,7 @@ const rhoaster = require('rhoaster');
 
 const testEnvironment = rhoaster({
   deploymentName: 'nodejs-rest-http-redhat',
-  dockerImage: 'registry.access.redhat.com/rhoar-nodejs/nodejs-8'
+  dockerImage: 'registry.access.redhat.com/rhscl/nodejs-8-rhel7'
 });
 
 testEnvironment.deploy()
